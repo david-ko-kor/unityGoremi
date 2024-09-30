b5b43b63b07db2ff31207cc540afb8327cbcd923
david-ko-kor <75867179+david-ko-kor@users.noreply.github.com>
2024-09-30 11:08:16 +0900

    newbrench
    this is first brench

--- Assets/goremi.cs
+++ Assets/goremi.cs
@@ -2,12 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HELLO : MonoBehaviour
+public class goremi : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("-");
+        Debug.Log("고규형");
+        Debug.Log("고규형");
     }
 
     // Update is called once per frame
